1e2eab0aeb3e3f78ea9f8cf72289226d898fe35d
DasKraken <42367146+DasKraken@users.noreply.github.com>
2019-07-09 12:23:29 +0200

    Randomize temporary folder folder name (closes #18)

--- src/Utils.js
+++ src/Utils.js
@@ -37,6 +37,14 @@ function formatScene(s) {
     return s;
 }
 
+function makeid(length) {
+    var result           = '';
+    var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
 
-
-module.exports = { pad, deleteFolderRecursive, toFilename, formatScene };
+module.exports = { pad, deleteFolderRecursive, toFilename, formatScene, makeid };
